8f0fda1ade3cd125cf722ba535e75052e4f33ca9
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 22:24:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat (array, array2)
+  [array].concat([array2])
+end
+
+def using_insert(array)
+
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(array)
+
+end
+
+def using_delete_at(array)
+
+end
